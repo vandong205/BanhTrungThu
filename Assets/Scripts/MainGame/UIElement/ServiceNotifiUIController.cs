9b65f346696f26b0a510c0b16b147fad47031c27
vandong205 <dong2000113@gmail.com>
2025-09-05 21:57:43 +0700

    05/09/2025 : UI Update

---
+++ Assets/Scripts/MainGame/UIElement/ServiceNotifiUIController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ServiceNotifiUIController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
